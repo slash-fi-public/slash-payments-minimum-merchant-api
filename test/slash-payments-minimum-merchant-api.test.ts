d83604fa3473c8b33cb9227206f27e1846352860
八木達也 <yagitatsuya@yagitatsuyas-MacBook-Air.local>
2022-08-13 19:01:51 +0900

    Initial commit

---
+++ test/slash-payments-minimum-merchant-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SlashPaymentsMinimumMerchantApi from '../lib/slash-payments-minimum-merchant-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/slash-payments-minimum-merchant-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SlashPaymentsMinimumMerchantApi.SlashPaymentsMinimumMerchantApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
